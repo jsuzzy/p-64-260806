fd2b501f4d211fe0bd3c8f0a4d3933bfb1d746f7
jeongsuji <sujijeong11@gmail.com>
2026-08-06 12:02:38 +0900

    lazy

--- src/main/java/com/back/p64260806/AppConfig.java
+++ src/main/java/com/back/p64260806/AppConfig.java
@@ -1,54 +1,35 @@
 package com.back.p64260806;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public PersonRepository personRepository() {
-        //어떤 객체는 초기화 작업을 매우 복잡하게 해야하는 경우가 있다.
-        //빈으로등록해서 스프링부트한테 맡기면 복잡한 생성 작업 어려울 수 있음
-        //세심하게 빈을 관리하고 싶으면 @Bean 방식이 좋음
-        //단순하게 관리하고 싶으면 @Component 방식이 좋음
-        return new PersonRepository(1);
-    }
-
-    @Bean
-    public PersonRepository personRepositoryV2() {
-        return new PersonRepository(2);
-    }
+    @Autowired
+    @Lazy
+    private AppConfig self;
 
     @Bean
     public ApplicationRunner myApplicationRunner3() {
         return args -> {
-            work1();
-            work2();
+            self.work1();
+            self.work2();
         };
     }
 
+    @Transactional
     public void work1() {
         System.out.println("work1");
     }
 
+    @Transactional
     public void work2() {
         System.out.println("work2");
     }
 
-    @Bean
-    @Order(1)
-    public ApplicationRunner myApplicationRunner1(){
-        System.out.println("myApplicationRunner1");
-        return new MyApplicationRunner(1);
-    }
-
-    @Bean
-    @Order(2)
-    public ApplicationRunner myApplicationRunner2(){
-        System.out.println("myApplicationRunner2");
-        return new MyApplicationRunner(2);
-    }
 }
--- src/main/java/com/back/p64260806/PersonRepository.java
+++ src/main/java/com/back/p64260806/PersonRepository.java
@@ -1,15 +1,14 @@
 package com.back.p64260806;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 @RequiredArgsConstructor
 public class PersonRepository {
 
-    private final int version;
-
     public int count() {
-
-        System.out.println("version = " + version);
         return 3;
     }
 
