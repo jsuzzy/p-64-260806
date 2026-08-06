a1c43d108f6d10460ea66a04ef081c9bf22011ac
jeongsuji <sujijeong11@gmail.com>
2026-08-06 11:36:01 +0900

    MyApplicationRunner 람다 적용

--- src/main/java/com/back/p64260806/AppConfig.java
+++ src/main/java/com/back/p64260806/AppConfig.java
@@ -1,16 +1,11 @@
 package com.back.p64260806;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
-//    @Bean
-//    public PersonService personService() {
-//        System.out.println("AppConfig.personService 호출됨!");
-//        return new PersonService();
-//    }
 
     @Bean
     public PersonRepository personRepository() {
@@ -24,5 +19,21 @@ public class AppConfig {
     @Bean
     public PersonRepository personRepositoryV2() {
         return new PersonRepository(2);
+    }
+
+    @Bean
+    public ApplicationRunner myApplicationRunner3() {
+        return args -> {
+            work1();
+            work2();
+        };
+    }
+
+    public void work1() {
+        System.out.println("work1");
+    }
+
+    public void work2() {
+        System.out.println("work2");
     }
 }
